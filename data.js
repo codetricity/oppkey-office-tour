6568e8d405255d3b2ee45d996dbef09326b60d8e
Craig <craig@oppkey.com>
2022-04-21 11:11:38 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,256 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 0.7772859785414958,
+        "pitch": 0.349198757644011,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9070474142332401,
+          "pitch": 0.16003802363429287,
+          "rotation": 0,
+          "target": "1-between-kitchen-and-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-between-kitchen-and-reception",
+      "name": "between kitchen and reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 2.3656203401353206,
+        "pitch": 0.11496304890823694,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.582382905006204,
+          "pitch": 0.41234827671141083,
+          "rotation": 6.283185307179586,
+          "target": "2-clock-outside-conference-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-clock-outside-conference-room",
+      "name": "clock outside conference room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8445232006017385,
+          "pitch": -0.1366703248243688,
+          "rotation": 4.71238898038469,
+          "target": "3-outside-of-main-conference-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-outside-of-main-conference-room",
+      "name": "Outside of Main Conference room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -0.5941707844832624,
+        "pitch": 0.327023641847326,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.453878634490895,
+          "pitch": 0.26011165705387995,
+          "rotation": 0,
+          "target": "4-soda-machine-and-copy-center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-soda-machine-and-copy-center",
+      "name": "soda machine and copy center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6439667200665227,
+          "pitch": 0.10227369539528297,
+          "rotation": 4.71238898038469,
+          "target": "5-hallway-to-private-offices"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hallway-to-private-offices",
+      "name": "hallway to private offices",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Oppkey Palo Alto Offices",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
